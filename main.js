13352a95362264a35958b9376310998a4356ff9d
Shaar <shaar@MacBook-Pro.local>
2022-06-12 23:01:49 -0400

    Adding Slideshow

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let image = document.getElementById("image");
 
-let images = ["images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png"];
+let images = ["images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png","images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png","images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png","images/E-commerce/1.png", "images/E-commerce/2.png"];
 
-setInterval(function() {
-    let random = Math.floor(Math.random() * 4);
+
+
+function slideshow(){
+    let random = Math.floor(Math.random() * 14);
     image.src = images[random];
-    
-}, 500);
+    setTimeout(slideshow, 5000);
+}
+
+slideshow();
