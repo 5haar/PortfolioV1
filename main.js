119056bec69dd79c02cc04f19b12e2cfbdf244b2
Shaar <shaar@MacBook-Pro.local>
2022-06-12 21:01:40 -0400

    Finished Tech Section

---
+++ main.js
@@ -0,0 +1,9 @@
+let image = document.getElementById("image");
+
+let images = ["images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png"];
+
+setInterval(function() {
+    let random = Math.floor(Math.random() * 4);
+    image.src = images[random];
+    
+}, 5000);
