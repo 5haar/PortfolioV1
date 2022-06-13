3fd86578bb9f79c82b09d7b0711de0e0e9593cfb
Shaar <shaar@MacBook-Pro.local>
2022-06-12 23:08:09 -0400

    aws fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let image = document.getElementById("image");
 
-let images = ["images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png","images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png","images/E-commerce/1.png", "images/E-commerce/2.png", "images/E-commerce/3.png", "images/E-commerce/4.png","images/E-commerce/1.png", "images/E-commerce/2.png"];
+let images = ["images/1.png", "images/2.png", "images/3.png", "images/4.png","images/1.png", "images/2.png", "images/3.png", "images/4.png","images/1.png", "images/2.png", "images/3.png", "images/4.png","images/1.png", "images/2.png"];
 
 
 
